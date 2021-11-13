792f54220163acffd7a501582b5d8db6de44e183
Big-Aaron <lixino727@qq.com>
2021-11-14 00:08:40 +0800

    更新完全背包和多重背包

--- Forecast/动态规划/背包问题/backpack_complete.c
+++ Forecast/动态规划/背包问题/backpack_complete.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 
+// 先遍历背包
+#define pack
+// 先遍历物品
+#define good
+
 int weight[] = {1, 3, 4};// 物品重量
 int value[] = {15, 20, 30};// 物品价值
 int bagWeight = 4;// 背包容量
@@ -15,12 +20,24 @@ void test_1_wei_bag_problem() {
 
     // 初始化
     memset(dp, 0, sizeof(dp));
-
+#ifdef good
+//先遍历物品
     for (int i = 0; i < num_of_goods; i++) { // 遍历物品,从物品 0 开始
-        for (int j = bagWeight; j >= weight[i]; j--) { // 遍历背包容量，从后往前
+        for (int j = weight[i]; j <= bagWeight; j++) { // 遍历背包容量，从前往后
             dp[j] = MAX(dp[j], dp[j - weight[i]] + value[i]);
         }
     }
+#endif
+#ifdef pack
+//先遍历背包
+    for (int j = 0; j <= bagWeight; j++) { //遍历背包容量，从前往后
+        for (int i = 0; i < num_of_goods; i++) {// 遍历物品,从物品 0 开始
+            if (j >= weight[i]) {
+                dp[j] = MAX(dp[j], dp[j - weight[i]] + value[i]);
+            }
+        }
+    }
+#endif
     printf("最大价值为:%d\n", dp[bagWeight]);
 }
 
--- Forecast/动态规划/背包问题/backpack_multiple.c
+++ Forecast/动态规划/背包问题/backpack_multiple.c
@@ -3,13 +3,14 @@
 
 int weight[] = {1, 3, 4};// 物品重量
 int value[] = {15, 20, 30};// 物品价值
-int bagWeight = 4;// 背包容量
+int num[] = {2, 3, 2};// 物品数量
+int bagWeight = 10;// 背包容量
 
 #define MAX(x, y) (x) > (y) ? (x) : (y);
 
 void test_1_wei_bag_problem() {
 
-    int num_of_goods = sizeof(weight) / sizeof(int);
+    int num_of_goods = 7;
 
     int dp[bagWeight + 1];// 一维滚动数组
 
@@ -18,7 +19,10 @@ void test_1_wei_bag_problem() {
 
     for (int i = 0; i < num_of_goods; i++) { // 遍历物品,从物品 0 开始
         for (int j = bagWeight; j >= weight[i]; j--) { // 遍历背包容量，从后往前
-            dp[j] = MAX(dp[j], dp[j - weight[i]] + value[i]);
+            for (int k = 1; k <= num[i] && (j - k * weight[i]) >= 0; ++k) {// 遍历相同的物品
+                dp[j] = MAX(dp[j], dp[j - k * weight[i]] + k * value[i]);
+            }
+
         }
     }
     printf("最大价值为:%d\n", dp[bagWeight]);
